23f986b98e2b0f54cedef643fd36edc6d84186cf
thuccai <48472060+thuccai@users.noreply.github.com>
2024-03-24 17:17:00 +0700

    Update test and home

---
+++ TracNghiemOnline/wwwroot/js/Test.js
@@ -0,0 +1,52 @@
+﻿
+    var form = document.getElementById("form");
+    var minute = document.getElementById("minute");
+    var second = document.getElementById("second");
+    var issubmit = false;
+
+    form.addEventListener('submit', function CheckSucess(event ) {
+        var result = confirm("Bạn đã có muốn nộp bài không ?");
+    if (result == false) {
+
+        event.preventDefault();
+        }
+
+    });
+
+    function submitForm() {
+        if (issubmit == true) {
+        form.submit();
+        }
+
+    }
+
+
+    function SetTimeTest() {
+        var minuteInt = parseInt(minute.innerText);
+    var secondInt = parseInt(second.innerText);
+
+
+    if (secondInt == 0) {
+        minuteInt--;
+    secondInt = 60;
+        }
+
+    secondInt--;
+    minute.innerHTML = minuteInt.toString();
+    if (secondInt < 10) {
+        second.innerHTML = "0" + secondInt.toString();
+        }
+    else {
+        second.innerHTML = secondInt.toString();
+        }
+
+    if (minute == 0 && second == 0) {
+        issubmit = true;
+    submitForm();
+        }
+
+
+    };
+
+    setInterval(SetTimeTest, 1000);
+
